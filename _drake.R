381414ba9e47df38b4616170c51590582f89e97c
Matthew Heun <matthew.heun@me.com>
2020-02-12 10:41:19 +0000

    Add _drake.R file to repo.

---
+++ _drake.R
@@ -0,0 +1,21 @@
+# This file serves the r_*() functions (e.g. r_make()) documented at
+# https://books.ropensci.org/drake/projects.html#safer-interactivity # nolint
+# and
+# https://docs.ropensci.org/drake/reference/r_make.html
+
+# Load your packages and supporting functions into your session.
+# If you use supporting scripts like the ones below,
+# you will need to supply them yourself. Examples:
+# https://github.com/wlandau/drake-examples/tree/master/main/R
+source("R/packages.R")  # Load your packages, e.g. library(drake).
+source("R/functions.R") # Define your custom code as a bunch of functions.
+source("R/plan.R")      # Create your drake plan.
+
+# _drake.R must end with a call to drake_config().
+# The arguments to drake_config() are basically the same as those to make().
+# options(clustermq.scheduler = "multicore") # For parallel computing.
+drake_config(
+  plan 
+  # parallelism = "clustermq",
+  # jobs = 16
+)
