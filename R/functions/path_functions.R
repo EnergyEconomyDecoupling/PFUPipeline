f6eef3c87aad60e60d6780f785c97c97aafae5bb
Matthew Heun <matthew.heun@me.com>
2020-02-12 15:44:57 +0000

    New file path_functions.R

--- R/functions/path_functions.R
+++ R/functions/path_functions.R
@@ -8,6 +8,8 @@
 #' `iea_folder` (the path to a folder containing IEA data inside of `data_folder`),
 #' `oecd_path` (the path to the IEA data for the OECD countries), and
 #' `nonoecd_path` (the path to the IEA data for non-OECD countries).
+#' 
+#' @export
 #'
 #' @examples
 #' get_paths()
@@ -22,14 +24,50 @@ get_paths <- function(home_path = file.path(Sys.getenv("HOME")), data_relpath = 
        nonoecd_path = nonoecd_path(iea_path))
 }
 
+#' Get path to IEA subfolder inside data folder
+#' 
+#' If the IEA data are included in the data_folder directly, `iea_relpath` can be set to the empty string `""`.
+#' 
+#' @param data_folder path to the data folder
+#' @param iea_relpath the relative path to the IEA data folder inside `data_folder`
+#'
+#' @return the full path to the IEA data folder inside `data_folder`
+#' 
+#' @export
+#'
+#' @examples
+#' iea_subfolder(file.path(get_paths()[["data_path"]], "Fellowship 1960-2015 PFU database"))
+#' iea_subfolder(file.path(get_paths()[["data_path"]], "Fellowship 1960-2015 PFU database"), "")
 iea_subfolder <- function(data_folder, iea_relpath = file.path("IEA extended energy data", "IEA 2015 energy balance data")) {
   file.path(data_folder, iea_relpath)
 }
 
+#' Get path to file containing OECD data
+#'
+#' @param iea_folder the path to the IEA data folder
+#' @param oecd_filename the name of the OECD data file inside `iea_folder`
+#'
+#' @return the full path to the IEA data for OECD countries
+#' 
+#' @export
+#'
+#' @examples
+#' oecd_path(get_paths()[["iea_path"]])
 oecd_path <- function(iea_folder, oecd_filename = "energy-balances-oecd-extended-energy.csv") {
   file.path(iea_folder, oecd_filename)
 }
 
+#' Get path to file containing non-OECD data
+#' 
+#' @param iea_folder the path to the IEA data folder
+#' @param nonoecd_filename the name of the non-OECD data file inside `iea_folder`
+#'
+#' @return the full path to the IEA data for non-OECD countries
+#' 
+#' @export
+#'
+#' @examples
+#' nonoecd_path(get_paths()[["iea_path"]])
 nonoecd_path <- function(iea_folder, nonoecd_filename = "energy-balances-nonoecd-extended-energy.csv") {
   file.path(iea_folder, nonoecd_filename)
 }
--- _drake.R
+++ _drake.R
@@ -7,9 +7,10 @@
 # If you use supporting scripts like the ones below,
 # you will need to supply them yourself. Examples:
 # https://github.com/wlandau/drake-examples/tree/master/main/R
-source("R/packages.R")  # Load your packages, e.g. library(drake).
-source("R/functions.R") # Define your custom code as a bunch of functions.
-source("R/plan.R")      # Create your drake plan.
+source("R/packages.R")                 # Load your packages, e.g. library(drake).
+source("R/functions/path_functions.R") # Define your custom code as a bunch of functions.
+source("R/functions/data_functions.R")
+source("R/plan.R")                     # Create your drake plan.
 
 # _drake.R must end with a call to drake_config().
 # The arguments to drake_config() are basically the same as those to make().
