5edf2474b82b0795dc42836740e1180e0d44ded2
Matthew Heun <matthew.heun@me.com>
2020-02-12 14:56:41 +0000

    Added a get_paths function

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,15 @@
 # Your custom code is a bunch of functions.
-# create_plot <- function(data) {
-#   ggplot(data, aes(x = Petal.Width, fill = Species)) +
-#     geom_histogram(binwidth = 0.25) +
-#     theme_gray(20)
-# }
+
+get_paths <- function() {
+  home_folder <- file.path(Sys.getenv("HOME"))
+  shared_folder <- file.path(home_folder, "Dropbox", "Fellowship 1960-2015 PFU database")
+  iea_folder <- file.path(shared_folder, "IEA extended energy data", "IEA 2015 energy balance data")
+  oecd_path <- file.path(iea_folder, "energy-balances-oecd-extended-energy.csv")
+  nonoecd_path <- file.path(iea_folder, "energy-balances-nonoecd-extended-energy.csv")
+  
+  list(home_folder = home_folder, 
+       shared_folder = shared_folder, 
+       iea_folder = iea_folder, 
+       oecd_path = oecd_path, 
+       nonoecd_path = nonoecd_path)
+}
