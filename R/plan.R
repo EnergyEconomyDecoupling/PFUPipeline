10bb25b3987e81c1a69e7ed03ce381140f6e716a
Matthew Heun <matthew.heun@me.com>
2020-02-12 16:26:46 +0000

    Now subsetting country data.

--- R/plan.R
+++ R/plan.R
@@ -7,6 +7,8 @@
 #   * command-option-control-V to trigger "Visualize a drake workflow".
 # --- MKH, 12 Feb 2020
 plan <- drake_plan(
-  countries = c("GH"),
-  paths = get_paths()
+  countries = c("ESP", "USA"),
+  paths = get_paths(), 
+  AllIEAData = load_IEA_data(paths$oecd_path, paths$nonoecd_path), 
+  CountryData = target(extract_country_data(AllIEAData, countries), dynamic = map(countries))
 )
