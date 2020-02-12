07d0c2678288c680940db65fbcc7e3c4c33cf87f
Matthew Heun <matthew.heun@me.com>
2020-02-12 14:56:55 +0000

    get_paths is now in the plan

--- R/plan.R
+++ R/plan.R
@@ -1,4 +1,12 @@
 # The workflow plan data frame outlines what you are going to do.
+
+# You may want to set a keyboard shortcut for executing this drake plan.
+# See Tools|Modify keyboard shortcuts...
+# I set 
+#   * command-option-control-D to trigger "Run a drake workflow" and 
+#   * command-option-control-V to trigger "Visualize a drake workflow".
+# --- MKH, 12 Feb 2020
 plan <- drake_plan(
-  countries = c("GH")
+  countries = c("GH"),
+  paths = get_paths()
 )
