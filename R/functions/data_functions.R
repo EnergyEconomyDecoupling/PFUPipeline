1e911e6f371850b74552feb3d60b1f4a49712712
Matthew Heun <matthew.heun@me.com>
2020-02-12 16:54:06 +0000

    Added documentation for the extract_country_data function.

--- R/functions/data_functions.R
+++ R/functions/data_functions.R
@@ -19,6 +19,17 @@ load_IEA_data <- function(oecd_path, nonoecd_path) {
   # dplyr::bind_rows(oecd, nonoecd)
 }
 
+#' Extract specific country data from AllIEAData
+#' 
+#' Data is extracted according to the `countries` object.
+#' ``dplyr::filter()`` does the subsetting.
+#'
+#' @param AllIEAData a data frame containing cleaned IEA extended energy balance data
+#' @param countries a list of 3-letter country codes for countries to be analyzed
+#'
+#' @return a data frame with the desired IEA data only
+#' 
+#' @export
 extract_country_data <- function(AllIEAData, countries) {
-  dplyr::filter(AllIEAData, Country %in% countries)
+  filter(AllIEAData, Country %in% countries)
 }
