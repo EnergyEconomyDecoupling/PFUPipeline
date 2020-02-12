14bcfcd5ff4bf4b10f4b16863a248892de00d0c5
Matthew Heun <matthew.heun@me.com>
2020-02-12 10:41:31 +0000

    Add countries list to plan.

--- R/plan.R
+++ R/plan.R
@@ -1,14 +1,4 @@
 # The workflow plan data frame outlines what you are going to do.
 plan <- drake_plan(
-  countries = c("GH"),
-  raw_data = readxl::read_excel(file_in("raw_data.xlsx")),
-  data = raw_data %>%
-    mutate(Species = forcats::fct_inorder(Species)),
-  hist = create_plot(data),
-  fit = lm(Sepal.Width ~ Petal.Width + Species, data),
-  report = rmarkdown::render(
-    knitr_in("report.Rmd"),
-    output_file = file_out("report.html"),
-    quiet = TRUE
-  )
+  countries = c("GH")
 )
