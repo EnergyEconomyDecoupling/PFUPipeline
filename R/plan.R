9e6b51d9c02214d419b717a9482d57ee9c866211
Matthew Heun <matthew.heun@me.com>
2020-02-12 20:18:48 +0000

    Deleted boilerplante comments. Now .trace-ing countries

--- R/plan.R
+++ R/plan.R
@@ -1,14 +1,8 @@
 # The workflow plan data frame outlines what you are going to do.
 
-# You may want to set a keyboard shortcut for executing this drake plan.
-# See Tools|Modify keyboard shortcuts...
-# I set 
-#   * command-option-control-D to trigger "Run a drake workflow" and 
-#   * command-option-control-V to trigger "Visualize a drake workflow".
-# --- MKH, 12 Feb 2020
 plan <- drake_plan(
   countries = c("ESP", "USA"),
   paths = get_paths(), 
   AllIEAData = load_IEA_data(paths$oecd_path, paths$nonoecd_path), 
-  CountryData = target(extract_country_data(AllIEAData, countries), dynamic = map(countries))
+  CountryData = target(extract_country_data(AllIEAData, countries), dynamic = map(countries, .trace = countries))
 )
