8c24ff09cf303a3ff2861079529546dd5bc53b68
Matthew Heun <matthew.heun@me.com>
2020-02-13 21:37:28 +0000

    get_paths --> get_abs_paths

--- R/plan.R
+++ R/plan.R
@@ -2,7 +2,7 @@
 
 plan <- drake_plan(
   countries = c("ESP", "USA"),
-  paths = get_paths(), 
+  paths = get_abs_paths(), 
   AllIEAData = load_IEA_data(paths$oecd_path, paths$nonoecd_path), 
   CountryData = target(extract_country_data(AllIEAData, countries), dynamic = map(countries, .trace = countries))
 )
