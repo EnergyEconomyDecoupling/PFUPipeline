2abe9f9d25e2a6a13cccd11d866a99a9555d8da1
Matthew Heun <matthew.heun@me.com>
2020-02-14 11:14:21 +0000

    Added CAN to list of countries.

--- R/plan.R
+++ R/plan.R
@@ -1,7 +1,7 @@
 # The workflow plan data frame outlines what you are going to do.
 
 plan <- drake_plan(
-  countries = c("ESP", "USA"),
+  countries = c("ESP", "USA", "CAN"),
   paths = get_abs_paths(), 
   AllIEAData = load_IEA_data(paths$oecd_path, paths$nonoecd_path), 
   CountryData = target(extract_country_data(AllIEAData, countries), dynamic = map(countries, .trace = countries))
