fbe5144f6a2c84b9d9bf20f97428466c93161e37
Matthew Heun <matthew.heun@me.com>
2020-02-15 20:20:04 +0000

    Now balancing data before specifying and PSUT. Also cleaned up code for balancing.

--- R/plan.R
+++ R/plan.R
@@ -5,8 +5,9 @@ plan <- drake_plan(
   paths = get_abs_paths(), 
   AllIEAData = load_tidy_iea_df(paths$iea_data_path),
   CountryIEAData = target(extract_country_data(AllIEAData, countries), dynamic = map(countries, .trace = countries)), 
-  Balances = target(calc_tidy_iea_df_balances(CountryIEAData), dynamic = map(countries, .trace = countries)), 
-  Balanced = target(tidy_iea_df_balanced(CountryIEAData), dynamic = map(countries, .trace = countries)), 
-  SpecifiedCountryIEAData = target(specify_all(CountryIEAData), dynamic = map(countries, .trace = countries)), 
-  PreppedSpecifiedCountryIEAData = target(prep_psut(SpecifiedCountryIEAData), dynamic = map(countries, .trace = countries)) 
+  BalancedBefore = target(tidy_iea_df_balanced(calc_tidy_iea_df_balances(CountryIEAData)), dynamic = map(countries, .trace = countries)), 
+  Balanced = target(fix_tidy_iea_df_balances(CountryIEAData), dynamic = map(countries, .trace = countries)), 
+  BalancedAfter = target(tidy_iea_df_balanced(calc_tidy_iea_df_balances(Balanced)), dynamic = map(countries, .trace = countries)), 
+  Specified = target(specify_all(Balanced), dynamic = map(countries, .trace = countries)), 
+  PSUT = target(prep_psut(Specified), dynamic = map(countries, .trace = countries)) 
 )
