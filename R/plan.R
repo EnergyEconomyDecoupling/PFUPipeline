d0289bbdf065d5e6480f7da94524bccfad32db23
Matthew Heun <matthew.heun@me.com>
2020-02-15 15:42:19 +0000

    Now making specified data.

--- R/plan.R
+++ R/plan.R
@@ -1,8 +1,11 @@
 # The workflow plan data frame outlines the analyses to be conducted.
 
 plan <- drake_plan(
-  countries = c("ESP"),
+  countries = c("ESP", "USA", "CAN"),
   paths = get_abs_paths(), 
   AllIEAData = load_tidy_iea_df(paths$iea_data_path),
-  CountryIEAData = target(extract_country_data(AllIEAData, countries), dynamic = map(countries, .trace = countries))
+  CountryIEAData = target(extract_country_data(AllIEAData, countries), dynamic = map(countries, .trace = countries)), 
+  Balances = target(calc_tidy_iea_df_balances(CountryIEAData), dynamic = map(countries, .trace = countries)), 
+  Balanced = target(tidy_iea_df_balanced(CountryIEAData), dynamic = map(countries, .trace = countries)), 
+  SpecifiedCountryIEAData = target(specify_all(CountryIEAData), dynamic = map(countries, .trace = countries))
 )
