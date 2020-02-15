cbd35f91499854c61338af49217f70382266b148
Matthew Heun <matthew.heun@me.com>
2020-02-15 15:27:51 +0000

    Now using load_tidy_iea_df to load IEA data. Everything is in one file.

--- R/plan.R
+++ R/plan.R
@@ -1,8 +1,8 @@
 # The workflow plan data frame outlines the analyses to be conducted.
 
 plan <- drake_plan(
-  countries = c("ESP", "USA", "CAN"),
+  countries = c("ESP"),
   paths = get_abs_paths(), 
-  AllIEAData = load_IEA_data(paths$oecd_path, paths$nonoecd_path), 
+  AllIEAData = load_tidy_iea_df(paths$iea_data_path),
   CountryIEAData = target(extract_country_data(AllIEAData, countries), dynamic = map(countries, .trace = countries))
 )
