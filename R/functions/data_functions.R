1d7548f6818081b4edda2b3adaa1af9e2e78fbc9
Matthew Heun <matthew.heun@me.com>
2020-02-15 15:28:18 +0000

    Delete load_IEA_data function. No longer needed now that all IEA data are in one file.

--- R/functions/data_functions.R
+++ R/functions/data_functions.R
@@ -1,24 +1,3 @@
-#' Read and clean IEA data
-#' 
-#' Loads the OECD and non-OECD data files and makes a single data frame.
-#' Internally, this function uses IEATools::load_tidy_iea_df().
-#'
-#' @param oecd_path the path to the OECD data file
-#' @param nonoecd_path the path to the non-OECD data file
-#'
-#' @return a single data frame containing all IEA data
-#' 
-#' @export
-#'
-#' @examples
-#' paths <- get_paths()
-#' load_all_IEA_data(paths$oecd_path, paths$nonoecd_path)
-load_IEA_data <- function(oecd_path, nonoecd_path) {
-  oecd <- load_tidy_iea_df(oecd_path)
-  nonoecd <- load_tidy_iea_df(nonoecd_path)
-  bind_rows(oecd, nonoecd)
-}
-
 #' Extract specific country data from AllIEAData
 #' 
 #' Data is extracted according to the `countries` object.
