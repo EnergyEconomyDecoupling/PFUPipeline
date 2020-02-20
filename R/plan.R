228e93fcc7186874215a030c9ecc6a103e0ca314
Matthew Heun <matthew.heun@me.com>
2020-02-20 07:40:49 +0000

    Improved names in plan.

--- R/plan.R
+++ R/plan.R
@@ -8,23 +8,24 @@ plan <- drake_plan(
   AllIEAData = load_tidy_iea_df(paths$iea_data_path),
   # Narrow down to only the countries of interest to us
   # and group in preparation for fixing energy balances
-  CountryIEAData = target(extract_country_data(AllIEAData, countries, max_year) %>% 
-                            group_by(Country, Method, Energy.type, Last.stage, Year, Product),
-                          dynamic = map(countries, .trace = countries)), 
+  IEAData = target(extract_country_data(AllIEAData, countries, max_year) %>% 
+                     group_by(Country, Method, Energy.type, Last.stage, Year, Product),
+                   dynamic = map(countries, .trace = countries)), 
   # Check whether energy products are balanced.
   # They're not. Use readd(BalancedBefore) to see the results.  
   # FALSE indicates a country with at least one balance problem.
-  BalancedBefore = target(CountryIEAData %>% calc_tidy_iea_df_balances() %>% tidy_iea_df_balanced(), 
+  BalancedBefore = target(IEAData %>% calc_tidy_iea_df_balances() %>% tidy_iea_df_balanced(), 
     dynamic = map(countries, .trace = countries)), 
   # Balance all of the data by product and year.
-  Balanced = target(fix_tidy_iea_df_balances(CountryIEAData), dynamic = map(countries, .trace = countries)), 
+  BalancedIEAData = target(fix_tidy_iea_df_balances(IEAData), dynamic = map(countries, .trace = countries)), 
   # Check that everything is balanced.
   # Use readd(BalancedAfter) to show that flows are balanced for each country.
   # TRUE indicates balance has been achieved.
-  BalancedAfter = target(Balanced %>% calc_tidy_iea_df_balances() %>% tidy_iea_df_balanced(), 
-                          dynamic = map(countries, .trace = countries)), 
-  # Specify the Balanced data frame by being more careful with names, etc.
-  Specified = target(specify_all(Balanced), dynamic = map(countries, .trace = countries)), 
+  BalancedAfter = target(BalancedIEAData %>% calc_tidy_iea_df_balances() %>% tidy_iea_df_balanced(), 
+                         dynamic = map(countries, .trace = countries)), 
+  OKToProceed = stopifnot(all(BalancedAfter)),
+  # Specify the BalancedIEAData data frame by being more careful with names, etc.
+  Specified = target(specify_all(BalancedIEAData), dynamic = map(countries, .trace = countries)), 
   # Arrange all the data into PSUT matrices.
   PSUT = target(prep_psut(Specified), dynamic = map(countries, .trace = countries)) 
 )
