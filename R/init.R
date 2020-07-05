68e69043ea79ce68b466222039e2bc59cee68c1f
Matthew Heun <matthew.heun@me.com>
2020-07-04 20:10:23 -0400

    Rearranging to put all user-changable things on init.R

--- R/init.R
+++ R/init.R
@@ -1,6 +1,11 @@
 # This script loads the functions for this drake repository by sourcing relevant files
 
+# Setup
 source("R/packages.R")                     # Load your packages, e.g. library(drake).
 source("R/functions/path_functions.R")     # Define your custom code as a bunch of functions.
-source("R/plan.R")                         # Create your drake plan.
 
+countries <- c("ESP")
+max_year <- 2017
+paths <- get_abs_paths()
+
+source("R/plan.R")                         # Create the drake plan.
--- R/plan.R
+++ R/plan.R
@@ -1,11 +1,9 @@
 # The workflow plan data frame outlines the analyses to be conducted.
 
-# Get a set of machine-specific paths
-paths <- get_abs_paths()
 
 # Create our plan
-plan <- SEAPSUTWorkflow::get_plan(countries = "ESP", 
-                                  max_year = 2017,
+plan <- SEAPSUTWorkflow::get_plan(countries = countries, 
+                                  max_year = max_year,
                                   iea_data_path = paths$iea_data_path, 
                                   exemplar_table_path = paths$exemplar_table_path, 
                                   fu_analysis_folder = paths$fu_analysis_folder)
