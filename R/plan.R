6a21fbaaf741689ba4d256640695f1229b65bdd4
Matthew Heun <matthew.heun@me.com>
2020-08-19 20:03:10 -0400

    Remove conflicts

--- R/plan.R
+++ R/plan.R
@@ -2,50 +2,9 @@
 
 paths <- get_abs_paths()
 
-<<<<<<< HEAD
-  countries = c("CHN","DNK","ESP","GBR","GHA","GRC","HND","IND","ISR","MAR","MEX","PRT","USA","World", "World marine bunkers", "World aviation bunkers"),
-  max_year = 2017,
-  paths = get_abs_paths(),
-  
-  # (1) Grab all the IEA data for ALL countries
-  AllIEAData = load_tidy_iea_df(paths$iea_data_path),
-  
-  # Narrow down to only the countries of interest to us
-  # and group in preparation for fixing energy balances
-  IEAData = target(extract_country_data(AllIEAData, countries, max_year), dynamic = map(countries)),
-  
-  # (2) Balance all the energy data. 
-  # First, check whether energy products are balanced. They're not. 
-  # FALSE indicates a country with at least one balance problem.
-  balanced_before = target(is_balanced(IEAData, countries), dynamic = map(countries)),
-  # Balance all of the data by product and year.
-  BalancedIEAData = target(make_balanced(IEAData, countries), dynamic = map(countries)),
-  # Check that everything is balanced after balancing.
-  balanced_after = target(is_balanced(BalancedIEAData, countries), dynamic = map(countries)),
-  # Don't continue if there is a problem.
-  OKToProceed = stopifnot(all(balanced_after)),
-  
-  # (3) Specify the BalancedIEAData data frame by being more careful with names, etc.
-  Specified = target(specify(BalancedIEAData, countries), dynamic = map(countries)),
-  
-  # (4) Arrange all the data into PSUT matrices with final stage data.
-  PSUT_final = target(make_psut(Specified, countries), dynamic = map(countries))
-  
-  # (5) Load allocation tables
-  
-  
-  # (6) Load efficiency tables
-  
-  
-  # (7) Extend to useful stage.  
-  
-  
-)
-=======
 # Create our drake plan
 plan <- SEAPSUTWorkflow::get_plan(countries = countries, 
                                   max_year = max_year,
                                   iea_data_path = paths$iea_data_path, 
                                   exemplar_table_path = paths$exemplar_table_path, 
                                   fu_analysis_folder = paths$fu_analysis_folder)
->>>>>>> develop
