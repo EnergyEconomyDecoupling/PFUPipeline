760eb0a560631e98351190cb37e13dfe404ebfc8
MatthewHeun <MatthewHeun@users.noreply.github.com>
2021-07-09 08:36:56 -0400

    Add machine_data_path and ceda_data_folder to debugging.R

--- R/debugging.R
+++ R/debugging.R
@@ -4,15 +4,17 @@
 # When done, don't forget to delete the .drake directory.
 
 
-# library(SEAPSUTWorkflow)
-# 
-# plan <- get_plan(countries = c("World", "ESP", "GRC"), 
-#                  max_year = 1971,
-#                  iea_data_path = "~/Dropbox/Fellowship 1960-2015 PFU database/IEA extended energy balance data/IEA 2019 energy balance data/IEA Extended Energy Balances 2019.csv",
-#                  exemplar_table_path = "~/Dropbox/Fellowship 1960-2015 PFU database/Database plan/Exemplar_Table.xlsx",
-#                  fu_analysis_folder = "~/Dropbox/Fellowship 1960-2015 PFU database/Country-level exergy accounting data", 
-#                  reports_source_folders = PFUSetup::get_abs_paths()[["reports_source_folders"]], 
-#                  reports_dest_folder = PFUSetup::get_abs_paths()[["reports_dest_folder"]])
-# 
-# 
-# drake::make(plan)
+library(SEAPSUTWorkflow)
+
+plan <- get_plan(countries = c("HND"),
+                 max_year = 1971,
+                 iea_data_path = "~/Dropbox/Fellowship 1960-2015 PFU database/IEA extended energy balance data/IEA 2019 energy balance data/IEA Extended Energy Balances 2019.csv",
+                 ceda_data_folder = file.path(PFUSetup::get_abs_paths()[["project_path"]], "Data", "CEDA Data"),
+                 machine_data_path = file.path(PFUSetup::get_abs_paths()[["project_path"]], "Data", "Machines - Data"),
+                 exemplar_table_path = "~/Dropbox/Fellowship 1960-2015 PFU database/Database plan/Exemplar_Table.xlsx",
+                 fu_analysis_folder = "~/Dropbox/Fellowship 1960-2015 PFU database/Country-level exergy accounting data",
+                 reports_source_folders = PFUSetup::get_abs_paths()[["reports_source_folders"]],
+                 reports_dest_folder = PFUSetup::get_abs_paths()[["reports_dest_folder"]])
+
+
+drake::make(plan)
