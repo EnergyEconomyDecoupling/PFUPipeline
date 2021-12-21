e791e358ece57a5b06b8836a385d391a3cbcc553
Matthew Heun <matthew.heun@me.com>
2021-12-21 13:47:50 -0500

    More countries

--- _drake.R
+++ _drake.R
@@ -13,8 +13,8 @@ library(SEAPSUTWorkflow)
 # Custom parameters
 max_year <- 2019                         # The last year to be analyzed
 
-# countries <- c("BRA", "CAN", "CHN", "DEU", "ESP", "GBR", "GHA", "GRC", "HKG", "HND", "IDN", "JPN", "IND", "JOR", "KOR", "MEX", "NOR", "RUS", "USA", "WMB", "WAB", "ZAF")
-countries <- c("WMB")
+countries <- c("BRA", "CAN", "CHN", "DEU", "ESP", "GBR", "GHA", "GRC", "HKG", "HND", "IDN", "JPN", "IND", "JOR", "KOR", "MEX", "NOR", "RUS", "USA", "WMB", "WAB", "ZAF")
+# countries <- c("WMB")
 
 
 additional_exemplars <- "WLD"
