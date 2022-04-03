ff48f6fc2d39c57c912f905e8966b73743c190b0
Matthew Heun <matthew.heun@me.com>
2022-04-03 12:07:31 -0400

    Building the pipeline.

--- R/agg_pipelines.R
+++ R/agg_pipelines.R
@@ -1,6 +1,41 @@
+#' Title
+#'
+#' @param countries A vector of abbreviations for countries whose energy conversion chain is to be analyzed,
+#'                  such as "c('GHA', 'ZAF')".
+#'                  Countries named in `countries` can also serve as exemplars for
+#'                  final-to-useful allocations and efficiencies.
+#' @param additional_exemplar_countries A vector of country abbreviations for which final-to-useful allocations
+#'                                      and efficiencies will be read.
+#'                                      An energy conversion chain will _not_ be constructed for these countries.
+#'                                      However, their final-to-useful allocations and efficiencies
+#'                                      may be used as exemplar information for the countries in `countries`.
+#'                                      Default is `NULL`, indicating no additional exemplars.
+#' @param years The years to be studied.
+#' @param how_far A string indicating the last target to include in the plan that is returned.
+#'                Default is "all_targets" to indicate all targets of the plan should be returned.
+#' @param iea_data_path The path to IEA extended energy balance data in .csv format.
+#' @param country_concordance_path The path to the country concordance Excel file.
+#' @param phi_constants_path The path to a phi (exergy-to-energy ratio) Excel file.
+#' @param ceda_data_folder The path to the CEDA data in text file, .per, format.
+#' @param machine_data_path The path to the machine data in .xlsx format.
+#' @param exemplar_table_path The path to an exemplar table.
+#' @param fu_analysis_folder The path to a folder containing final-to-useful analyses.
+#'                           Sub-folders named with 3-letter country abbreviations are assumed.
+#' @param reports_source_folders A string vector containing paths to folders of report sources, usually
+#'                               `.Rnw` or `.Rmd` files.
+#' @param reports_dest_folder The path to a folder into which reports are written.
+#' @param pipeline_caches_folder The path to a folder where .zip files of the pipeline cache folders are stored.
+#' @param pipeline_releases_folder The path to a folder where releases of important targets are stored
+#'                                 for later retrieval as pinned items on a pinboard..
+#' @param release A boolean that tells whether a new release of the `PSUT` target should be made.
+#'                Default is `FALSE`.
+#'
+#' @return A `targets` pipeline.
+#' 
+#' @export
 get_pipeline <- function(countries,
                          additional_exemplar_countries = NULL,
-                         max_year,
+                         years,
                          how_far = "all_targets",
                          iea_data_path,
                          country_concordance_path,
--- _targets.R
+++ _targets.R
@@ -4,3 +4,72 @@ library(targets)
 # targets::tar_make_clustermq(workers = 8) to execute across multiple cores.
 # targets::tar_read(<<target_name>>) to view the results.
 # targets::tar_destroy() to start over with everything,
+
+
+# Custom parameters
+years <- 2019                         # The years to be analyzed
+
+# countries <- c("BRA", "CAN", "CHN", "DEU", "ESP", "GBR", "GHA", "GRC", "HKG", "HND", "IDN", "JPN", "IND", "JOR", "KOR", "MEX", "NOR", "RUS", "USA", "WMB", "WAB", "ZAF")
+# countries <- c("WMB")
+# countries <- c("USA")
+# countries <- c("FSU", "YGS")
+# countries <- c("SUN", "YUG")
+# countries <- c("YGS")
+# countries <- setdiff(PFUWorkflow::canonical_countries, c("FSU", "FYG", "CIV")) |> as.character()
+countries <- setdiff(PFUWorkflow::canonical_countries, c("WRLD")) |> as.character()
+
+additional_exemplars <- "WRLD"
+
+# Number of machine cores to use.
+# Set to less than available on your machine.
+# Applies only to tar_make_clustermq().
+# To parallelize the execution of this pipeline, say
+# targets::tar_make_clustermq(workers = X),
+# where X is the same as the number of cores.
+# num_cores <- 3
+num_cores <- 8
+
+# Set the target to debug.  Set to NULL to turn off debugging.
+# To debug, set appropriate breakpoints and use
+# tar_make(callr_function = NULL).
+# debug_target <- "PSUT_Re_all_St_fu"
+debug_target <- NULL
+
+# Should we do a release of the results?
+release <- FALSE
+
+# End user-adjustable parameters.
+
+
+# Set up for multithreaded work on the local machine.
+options(clustermq.scheduler = "multiprocess")
+
+# Set options for the targets package.
+targets::tar_option_set(
+  
+  # Set the target to debug, if needed.
+  debug = debug_target,
+  
+  # Set packages to be used.
+  packages = c(
+    "dplyr",
+    "IEATools",
+    "parsedate",
+    "PFUAggDatabase",
+    "pins",
+    "tidyr"),
+  
+  # Set the number of cores for multiprocessing.
+  resources = targets::tar_resources(
+    clustermq = targets::tar_resources_clustermq(template = list(num_cores = num_cores))
+  )
+)
+
+
+# Pull in the pipeline
+PFUDatabase::get_pipeline(countries = countries,
+                          years = years,
+                          aggregation_maps_path = PFUSetup::get_abs_paths()[["aggregation_mapping_path"]],
+                          pipeline_caches_folder = PFUSetup::get_abs_paths()[["pipeline_caches_folder"]],
+                          pipeline_releases_folder = PFUSetup::get_abs_paths()[["pipeline_releases_folder"]],
+                          release = release)
