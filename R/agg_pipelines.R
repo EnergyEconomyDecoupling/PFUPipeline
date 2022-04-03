2f842a1ad025d3e0d3a07fdc4c6dbb6306d77561
Matthew Heun <matthew.heun@me.com>
2022-04-03 11:43:26 -0400

    First agg pipeline function

---
+++ R/agg_pipelines.R
@@ -0,0 +1,18 @@
+get_pipeline <- function(countries,
+                         additional_exemplar_countries = NULL,
+                         max_year,
+                         how_far = "all_targets",
+                         iea_data_path,
+                         country_concordance_path,
+                         phi_constants_path,
+                         ceda_data_folder,
+                         machine_data_path,
+                         exemplar_table_path,
+                         fu_analysis_folder,
+                         reports_source_folders,
+                         reports_dest_folder,
+                         pipeline_caches_folder,
+                         pipeline_releases_folder,
+                         release = FALSE) {
+  
+}
