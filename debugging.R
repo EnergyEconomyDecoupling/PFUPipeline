c0a139088d971c63757e633124190141feee1a6d
Matthew Heun <matthew.heun@me.com>
2022-04-04 08:21:03 -0400

    Update debugging file for new constants

--- debugging.R
+++ debugging.R
@@ -2,25 +2,25 @@
 # library(PFUWorkflow)
 # 
 # 
-# max_year <- 2019                        
+# max_year <- 2019
 # countries <- c("WMB")
 # additional_exemplars <- "WLD"
 # 
 # # Create our drake plan
-# plan <- PFUWorkflow::get_plan(countries = countries, 
-#                                   additional_exemplar_countries = additional_exemplars,
-#                                   max_year = max_year,
-#                                   iea_data_path = PFUSetup::get_abs_paths()[["iea_data_path"]],
-#                                   country_concordance_path = PFUSetup::get_abs_paths()[["country_concordance_path"]],
-#                                   phi_constants_path = PFUSetup::get_abs_paths()[["phi_constants_path"]],
-#                                   ceda_data_folder = PFUSetup::get_abs_paths()[["ceda_data_folder"]],
-#                                   machine_data_path = PFUSetup::get_abs_paths()[["machine_data_folder"]],
-#                                   exemplar_table_path = PFUSetup::get_abs_paths()[["exemplar_table_path"]],
-#                                   fu_analysis_folder = PFUSetup::get_abs_paths()[["fu_analysis_folder"]],
-#                                   reports_source_folders = PFUSetup::get_abs_paths()[["reports_source_folders"]],
-#                                   reports_dest_folder = PFUSetup::get_abs_paths()[["reports_dest_folder"]], 
-#                                   workflow_output_folder = PFUSetup::get_abs_paths()[["workflow_output_folder"]],
-#                                   workflow_releases_folder = PFUSetup::get_abs_paths()[["workflow_releases_folder"]], 
-#                                   release = FALSE)
+# plan <- PFUWorkflow::get_plan(countries = countries,
+#                               additional_exemplar_countries = additional_exemplars,
+#                               max_year = max_year,
+#                               iea_data_path = PFUSetup::get_abs_paths()[["iea_data_path"]],
+#                               country_concordance_path = PFUSetup::get_abs_paths()[["country_concordance_path"]],
+#                               phi_constants_path = PFUSetup::get_abs_paths()[["phi_constants_path"]],
+#                               ceda_data_folder = PFUSetup::get_abs_paths()[["ceda_data_folder"]],
+#                               machine_data_path = PFUSetup::get_abs_paths()[["machine_data_folder"]],
+#                               exemplar_table_path = PFUSetup::get_abs_paths()[["exemplar_table_path"]],
+#                               fu_analysis_folder = PFUSetup::get_abs_paths()[["fu_analysis_folder"]],
+#                               reports_source_folders = PFUSetup::get_abs_paths()[["reports_source_folders"]],
+#                               reports_dest_folder = PFUSetup::get_abs_paths()[["reports_dest_folder"]],
+#                               pipeline_caches_folder = PFUSetup::get_abs_paths()[["pipeline_caches_folder"]],
+#                               pipeline_releases_folder = PFUSetup::get_abs_paths()[["pipeline_releases_folder"]],
+#                               release = FALSE)
 # 
 # drake::make(plan)
