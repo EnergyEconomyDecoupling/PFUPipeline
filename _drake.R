fec377e4702dee26e9d78c7fb11a126872744283
Matthew Heun <matthew.heun@me.com>
2022-04-14 15:31:58 -0400

    Set up for canonical countries.

--- _drake.R
+++ _drake.R
@@ -24,9 +24,7 @@ max_year <- 2019                         # The last year to be analyzed
 # countries <- c("SUN", "YUG")
 # countries <- c("YGS")
 # countries <- setdiff(PFUWorkflow::canonical_countries, c("FSU", "FYG", "CIV")) |> as.character()
-# countries <- PFUWorkflow::canonical_countries |> as.character()
-# countries <- PFUWorkflow::canonical_countries[1:50]
-countries <- PFUWorkflow::canonical_countries[127:127]
+countries <- PFUWorkflow::canonical_countries |> as.character()
 
 additional_exemplars <- "WRLD"
 
