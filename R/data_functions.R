420cbecccac4f4392ed6ea332449c7a91d971b50
Matthew Heun <matthew.heun@me.com>
2023-06-16 09:01:19 -0400

    Now using funcs from PFUPipelineTools

--- R/data_functions.R
+++ R/data_functions.R
@@ -59,31 +59,31 @@ load_country_concordance_table <- function(country_concordance_path,
 #' IEATools::sample_iea_data_path() %>%
 #'   IEATools::load_tidy_iea_df() %>%
 #'   filter_countries_years(countries = c("ZAF"), years = 1960:1999)
-filter_countries_years <- function(.df, 
-                                   countries, 
-                                   years,
-                                   country = IEATools::iea_cols$country,
-                                   year = IEATools::iea_cols$year) {
-  countries1 <- length(countries) == 1
-  years1 <- length(years) == 1
-  if (countries1 & years1) {
-      if (countries == "all" & years == "all") {
-        return(.df)
-      }
-  }
-  if (countries1) {
-    if (countries == "all") {
-      return(.df %>% dplyr::filter(.data[[year]] %in% years))
-    }
-  }
-  if (years1) {
-    if (years == "all") {
-      return(.df %>% dplyr::filter(.data[[country]] %in% countries))
-    }
-  }
-  .df %>% 
-    dplyr::filter(.data[[country]] %in% countries, .data[[year]] %in% years)
-}
+# filter_countries_years <- function(.df, 
+#                                    countries, 
+#                                    years,
+#                                    country = IEATools::iea_cols$country,
+#                                    year = IEATools::iea_cols$year) {
+#   countries1 <- length(countries) == 1
+#   years1 <- length(years) == 1
+#   if (countries1 & years1) {
+#       if (countries == "all" & years == "all") {
+#         return(.df)
+#       }
+#   }
+#   if (countries1) {
+#     if (countries == "all") {
+#       return(.df %>% dplyr::filter(.data[[year]] %in% years))
+#     }
+#   }
+#   if (years1) {
+#     if (years == "all") {
+#       return(.df %>% dplyr::filter(.data[[country]] %in% countries))
+#     }
+#   }
+#   .df %>% 
+#     dplyr::filter(.data[[country]] %in% countries, .data[[year]] %in% years)
+# }
 
 
 #' Tells whether IEA data are balanced
